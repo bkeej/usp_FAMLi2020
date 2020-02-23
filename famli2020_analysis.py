195d306e80cfaf8eacaa5d92be31372c06aec15d
Robert Henderson <balamkej@gmail.com>
2020-02-23 16:13:22 -0700

    main()

--- famli2020_analysis.py
+++ famli2020_analysis.py
@@ -4,13 +4,25 @@ import os
 import csv 
 import xml.etree.ElementTree as ET 
 
-corpusDir = "uspanteko_corpus_xml"
-dataDir = "data_for_analysis"
+corpusDir = "uspanteko_corpus_xml/"
+dataDir = "data_for_analysis/"
 
 xmlFiles = os.listdir(corpusDir)
 
 def parseXML(xmlfile): 
-	pass
+	tree = ET.parse(xmlfile) 
+	root = tree.getroot() 
+	print root
 
 def savetoCSV():
 	pass
+
+# Main
+
+test = corpusDir + xmlFiles[0]
+
+def main():
+	parseXML(test)
+
+if __name__ == "__main__":
+	main() 
