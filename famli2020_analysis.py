2edbde1180e5f6617710e38c4eb144ffb121a040
Robert Henderson <balamkej@gmail.com>
2020-02-25 15:00:23 -0700

    working csv writer

--- famli2020_analysis.py
+++ famli2020_analysis.py
@@ -17,6 +17,21 @@ xml_files = os.listdir(corpus_dir)
 # Parsers
 #
 
+
+# # NOTE: Maybe we can be more general by skipping intermediate entries with an origin class. 
+# # Verbs don't have origin class in XML scheme
+# def vv_match(phrase):
+# 	verb_tags = ["VT", "VI"]
+# 	pos = phrase.findall("//pos") #return all pos tags under phrase
+# 	for p in pos:
+# 		if p.get("orig_cls") != None:
+# 			pass
+# 		else:
+# 			pos.remove(p)
+# 	for p, p' in zip(pos, pos[1:]): # trick to get adjacent pairs in phrase, i.e., pos tags
+# 		if x.get("text") in verb_tags and y.get("text") in verb_tags: # True if adjacent verb tags
+			
+
 ## Takes an IGT-XML file and returns a list of dictionaries characterizing 
 ## sentences with adjacent verbs prepped for adding to CSV.
 def parse_vv(xmlfile): 
@@ -32,8 +47,8 @@ def parse_vv(xmlfile):
 				row["phrase_id"] = phrase.get("ph_id")
 				row["v1"] = root.find("./body/morphemes/phrase/morph[@morph_id='" + x.get("morph_ref") + "']").get("text") 
 				row["v2"] = root.find("./body/morphemes/phrase/morph[@morph_id='" + y.get("morph_ref") + "']").get("text")
-				row["sentence"] = root.find("./body/phrases/phrase[@ph_id='" + phrase.get("ph_id") + "']/plaintext").text
-				row["translation"] = root.find("./body/translations/phrase[@ph_id='" + phrase.get("ph_id") + "']/trans").text
+				row["sentence"] = root.find("./body/phrases/phrase[@ph_id='" + phrase.get("ph_id") + "']/plaintext").text.strip()
+				row["translation"] = root.find("./body/translations/phrase[@ph_id='" + phrase.get("ph_id") + "']/trans").text.strip()
 				rows.append(row)
 	return rows 
 
@@ -59,11 +74,15 @@ def parse_no_pers(xmlfile):
 # Main
 #
 
-test = corpus_dir + xml_files[0]
+test = corpus_dir + xml_files[2]
 
 def main():
-	# print parse_vv(test)
-	print parse_no_pers(test)
+	with open(data_dir + "VV.csv", "w") as csvfile:
+		fieldnames = ["tx_title", "phrase_id", "v1", "v2", "sentence", "translation"]
+		writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+		writer.writeheader()
+		for row in parse_vv(test):
+			writer.writerow(row)
 
 if __name__ == "__main__":
 	main() 
