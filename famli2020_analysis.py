b57471a04b98e9930a623e86a25e89e27ac9b5ad
Robert Henderson <balamkej@gmail.com>
2020-02-25 08:50:04 -0700

    parsers return xml objects not ids

--- famli2020_analysis.py
+++ famli2020_analysis.py
@@ -17,36 +17,38 @@ xmlFiles = os.listdir(corpusDir)
 # Parsers
 #
 
-## Takes an IGT-XML file and returns a list of phrase ids of phrases that have adjacent verbs (ie., VI / VT)
+## Takes an IGT-XML file and returns a list of phrases that have adjacent verbs (ie., VI / VT)
 def parseVV(xmlfile): 
 	verbTags = ["VT", "VI"]
-	pharseMatch = []
+	parseMatch = []
 	tree = ET.parse(xmlfile) 
 	root = tree.getroot()
 	for phrase in root.findall("./body/postags/phrase"):
 		for x, y in zip(phrase, phrase[1:]): # trick to get adjacent pairs in phrase, i.e., pos tags
 			if x.get("text") in verbTags and y.get("text") in verbTags: # True if adjacent verb tags
-				pharseMatch.append(phrase.get("ph_id"))
-	return pharseMatch
+				parseMatch.append(phrase)
+	return parseMatch
 
-## Takes an IGT-XML file and returns a list of phrase ids of phrases with transitive verbs, but no person-marking.
+## Takes an IGT-XML file and returns a list of phrases with transitive verbs, but no person-marking.
 def parseNoPers(xmlfile):
-	pharseMatch = []
+	parseMatch = []
 	tree = ET.parse(xmlfile) 
 	root = tree.getroot()
 	for phrase in root.findall("./body/postags/phrase"):
 		for x, y in zip(phrase, phrase[1:]): # trick to get adject pairs in phrase, i.e., pos tags
 			if y.get("text") == "VT" and not x.get("text") == "PERS": # True if any VT ever not follows PERS
-				pharseMatch.append(phrase.get("ph_id"))
-	return pharseMatch
+				parseMatch.append(phrase)
+	return parseMatch
 
 #
 # Saving analysis 
 #
 
+# Takes a phrase element and returns a list of string to write to CSV 
 def vvToRow(phrase):
 	pass
 
+# Takes a phrase element and returns a list of string to write to CSV
 def noPersToRow(phrase):
 	pass
 
