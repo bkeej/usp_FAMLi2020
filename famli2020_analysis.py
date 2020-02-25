12a7e587b67cf159e85c3c4ee391b3b97522c558
Robert Henderson <balamkej@gmail.com>
2020-02-25 09:00:23 -0700

    dumb renaming

--- famli2020_analysis.py
+++ famli2020_analysis.py
@@ -8,62 +8,63 @@ import xml.etree.ElementTree as ET
 # Files and Directories
 #
 
-corpusDir = "uspanteko_corpus_xml/"
-dataDir = "data_for_analysis/"
+corpus_dir = "uspanteko_corpus_xml/"
+data_dir = "data_for_analysis/"
 
-xmlFiles = os.listdir(corpusDir)
+xml_files = os.listdir(corpus_dir)
 
 #
 # Parsers
 #
 
-## Takes an IGT-XML file and returns a list of phrases that have adjacent verbs (ie., VI / VT)
-def parseVV(xmlfile): 
-	verbTags = ["VT", "VI"]
-	parseMatch = []
+## Takes an IGT-XML file and returns a list of phrase objects that have adjacent verbs (ie., VI / VT)
+def parse_vv(xmlfile): 
+	verb_tags = ["VT", "VI"]
+	parse_match = []
 	tree = ET.parse(xmlfile) 
 	root = tree.getroot()
 	for phrase in root.findall("./body/postags/phrase"):
 		for x, y in zip(phrase, phrase[1:]): # trick to get adjacent pairs in phrase, i.e., pos tags
-			if x.get("text") in verbTags and y.get("text") in verbTags: # True if adjacent verb tags
-				parseMatch.append(phrase)
-	return parseMatch
+			if x.get("text") in verb_tags and y.get("text") in verb_tags: # True if adjacent verb tags
+				parse_match.append(phrase)
+	return parse_match
 
-## Takes an IGT-XML file and returns a list of phrases with transitive verbs, but no person-marking.
-def parseNoPers(xmlfile):
-	parseMatch = []
+## Takes an IGT-XML file and returns a list of phrase objects with transitive verbs, but no person-marking.
+def parse_no_pers(xmlfile):
+	parse_match = []
 	tree = ET.parse(xmlfile) 
 	root = tree.getroot()
 	for phrase in root.findall("./body/postags/phrase"):
 		for x, y in zip(phrase, phrase[1:]): # trick to get adject pairs in phrase, i.e., pos tags
 			if y.get("text") == "VT" and not x.get("text") == "PERS": # True if any VT ever not follows PERS
-				parseMatch.append(phrase)
-	return parseMatch
+				parse_match.append(phrase)
+	return parse_match
 
 #
 # Saving analysis 
 #
 
-# Takes a phrase element and returns a list of string to write to CSV 
-def vvToRow(phrase):
+# Takes a phrase object and returns a list of string to write to CSV 
+def vv_to_row(phrase):
 	pass
 
-# Takes a phrase element and returns a list of string to write to CSV
-def noPersToRow(phrase):
+# Takes a phrase object and returns a list of string to write to CSV
+def no_pers_to_row(phrase):
 	pass
 
-def saveToCSV():
-	pass
+def save_to_csv(file,rower):
+	with open(data_dir + file, mode 'w') as dataFile:
+
 
 #
 # Main
 #
 
-test = corpusDir + xmlFiles[0]
+test = corpus_dir + xml_files[0]
 
 def main():
-	print parseVV(test)
-	print parseNoPers(test)
+	print parse_vv(test)
+	print parse_no_pers(test)
 
 if __name__ == "__main__":
 	main() 
